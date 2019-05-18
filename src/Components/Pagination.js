e9f073fe73d727e066563256433940121e5a3475
Madhurgang <mdhrgngwn111@gmail.com>
2019-05-19 01:14:05 +0530

    complete ui for github user finder app

--- src/Components/Pagination.js
+++ src/Components/Pagination.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { Grid, Form, Pagination, Segment } from 'semantic-ui-react'
+import { Grid, Pagination} from 'semantic-ui-react'
 
 export default class MyPagination extends Component {
   state = {
